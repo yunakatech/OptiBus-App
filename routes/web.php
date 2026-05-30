f1d8e2e08125de965c3a57a85621e3fe6a92ada3
yunakatech <no-email@example.com>
2026-05-30 12:46:30 +0800

    Remove welcome page and redirect home to login

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,8 @@ use App\Http\Controllers\CharterDocumentController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LuggageDocumentController;
 use Illuminate\Support\Facades\Route;
-use Laravel\Fortify\Features;
 
-Route::inertia('/', 'Welcome', [
-    'canRegister' => Features::enabled(Features::registration()),
-])->name('home');
+Route::redirect('/', '/login')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', DashboardController::class)->name('dashboard');
